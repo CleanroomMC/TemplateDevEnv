c82fcee8aaffb3c511bbca78d0b1516f4b3c9b51
Rongmario <rongmario@protonmail.com>
2024-04-07 18:39:25 +0100

    Merge overhaul branch to be upstream (#24)
    * Debug mode for publishing artifacts
    * Jabel
    * Generation of mod meta, pack meta and mixin jsons
    * Fixed runObfServer using 1.7's main class rather than 1.12's
    - Allows changing of source without it being regenerated in dev
    * ExampleMod + fixing tag collection
    * Template expanding for mcmod.info + pack.mcmeta + remove redundant tasks
    - Now supports arbitrary script blocks to retrieve value `${{ }}` from directly in gradle.properties
    * Deployment via tasks/actions + changelog support + script folder
    * Fixed mixin json generating condition
    * Fix ATs not being applied
    * Allow mixinbooter & configanytime to be prioritized in obf runs
    * Remove redundant coremod arg addition as manifest is read at runtime
    * Allow processResources to work correctly
    * refactor: make parser changelog as method instead of job
    * fix: ensure correct header parser for changelog (2to2 and 3to3)
    * fix: no env available due to Github don't automatic inject env value to GHA
    * refactor: standardize mod version with SemVer, remove unnecessary changelog block in `build.gradle`
    * refactor: mixin config template and generator, resource filter
    * Updated Gradle to 8.7 + RetroFuturaGradle to 1.3.35
    * Update MixinBooter to 9.1 + provide wiki link
    
    Co-authored-by: Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
    Co-authored-by: Li <nhatlinh.l195@gmail.com>
    Co-authored-by: Li <li.hvktqs@gmail.com>

--- src/main/java/com/cleanroommc/README.md
+++
@@ -1,2 +0,0 @@
-- Here lies the root of the `io.github.cleanroommc` package, add another level with your mod id and use that as the root for your mod classes.
-
---
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -0,0 +1,24 @@
+package com.example.modid;
+
+import com.example.modid.Tags;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class ExampleMod {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    /**
+     * <a href="https://wiki.cleanroommc.com/mod-development/event/overview/">
+     *     Take a look at how many FMLStateEvents you can listen to via the @Mod.EventHandler annotation here
+     * </a>
+     */
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+    }
+
+}
